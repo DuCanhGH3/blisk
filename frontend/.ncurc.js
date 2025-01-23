c13458d6aa7e4b402c09cf45fb1784ca62c99974
DuCanhGH <ngoducanh2912@gmail.com>
2025-01-23 20:14:38 +0700

    chore(deps)

--- frontend/.ncurc.js
+++ frontend/.ncurc.js
@@ -6,9 +6,6 @@ module.exports = {
     return !filteredDependencies.includes(dependencyName);
   },
   target(dependencyName) {
-    if (dependencyName === "tailwindcss" || dependencyName === "@tailwindcss/vite") {
-      return "@next";
-    }
     if (dependencyName.includes("serwist")) {
       return "@preview";
     }
